c3b5ff0d1065cc10ba0513545a0498e40bd6585f
karchn <31886169+karchn@users.noreply.github.com>
2023-12-01 20:32:07 +0530

    Update PayGradePage.java

--- src/pageElements/PayGradePage.java
+++ src/pageElements/PayGradePage.java
@@ -9,7 +9,7 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-public class PayGradePage {
+public class PayGradePage extends AbstractMethods{
 
 	WebDriver driver;
 
@@ -54,7 +54,6 @@ public class PayGradePage {
 			catch(Exception e) {
 				
 			}
-			Thread.sleep(1000);
 			savebutton.click();
 		} 
 		catch (Exception e) {
