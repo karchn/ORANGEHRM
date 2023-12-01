ab310e60d1ba7822bb157b25bdb4ce33d7fea1a9
karchn <31886169+karchn@users.noreply.github.com>
2023-12-01 20:09:58 +0530

    Update LandingPage.java

--- src/pageElements/LandingPage.java
+++ src/pageElements/LandingPage.java
@@ -31,17 +31,19 @@ public class LandingPage extends AbstractMethods {
 		driver.manage().window().maximize();
 	}
 
-	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input")
+	@FindBy(xpath = "(//input[@class='oxd-input oxd-input--active'])[1]")
 	WebElement enterusername;
 
-	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input")
+	@FindBy(xpath = "(//input[@class='oxd-input oxd-input--active'])[2]")
 	WebElement enterpassword;
 
 	@FindBy(xpath = "//button[@type='submit']")
 	WebElement submitbutton;
 
+	By enterusername1 = By.xpath("(//input[@class='oxd-input oxd-input--active'])[1]");
+	
 	public UserManagement enterlogindetails(String username, String password) throws InterruptedException {
-		Thread.sleep(4000);
+		Wait_Till_Link_Is_Clickable(enterusername1);
 		enterusername.sendKeys(username);
 		enterpassword.sendKeys(password);
 		submitbutton.sendKeys(Keys.ENTER);
@@ -49,7 +51,7 @@ public class LandingPage extends AbstractMethods {
 		return um;
 	}
 
-	@FindBy(xpath = "//*[@id=\"app\"]/div[1]/div[1]/aside/nav/div[2]/ul/li[1]/a")
+	@FindBy(xpath = "(//a[@class='oxd-main-menu-item'])[1]")
 	WebElement Admin;
 
 	@FindBy(xpath = "//button[@class='oxd-button oxd-button--medium oxd-button--secondary']")
@@ -61,17 +63,21 @@ public class LandingPage extends AbstractMethods {
 	@FindBy(xpath = "//*[text()='Admin']")
 	WebElement selectadmin;
 
+	By clickonadd1 = By.xpath("//button[@class='oxd-button oxd-button--medium oxd-button--secondary']");
+	By userrole1 = By.xpath("//div[text()='-- Select --'][1]");
+	By selectadmin1 = By.xpath("//*[text()='Admin']");
+	
 	public void clickonadmin() throws InterruptedException {
 		Admin.click();
-		Thread.sleep(3000);
+		Wait_Till_Link_Is_Clickable(clickonadd1);
 		clickonadd.click();
 
 	}
 
 	public void adduser() throws InterruptedException {
-		Thread.sleep(3000);
+		Wait_Till_Link_Is_Clickable(userrole1);
 		userrole.click();
-		Thread.sleep(3000);
+		Wait_Till_Link_Is_Clickable(selectadmin1);
 		selectadmin.click();
 
 	}
