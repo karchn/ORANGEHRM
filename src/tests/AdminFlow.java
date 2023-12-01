eeb7efefa9d2f267f7d239f8a7cb6a7c59e0cc7a
karchn <31886169+karchn@users.noreply.github.com>
2023-12-01 22:17:58 +0530

    Update AdminFlow.java

--- src/tests/AdminFlow.java
+++ src/tests/AdminFlow.java
@@ -38,68 +38,86 @@ public class AdminFlow extends Basetest{
 	
 	@Test
 	public void adminmenuflow() throws Exception {
-	   lp=LaunchApplication();
-		
-	   //Thread.sleep(5000);
-       ExcelUtility1 xs = new ExcelUtility1();
-		
+		lp = LaunchApplication();
+
+		ExcelUtility1 xs = new ExcelUtility1();
+
 		String username = xs.Get_Username(1, 0).toString();
 		String password = xs.Get_password(1, 1).toString();
-		vp=lp.enterlogindetails(username,password);
-		
-		
-		//UserManagement_page
+		vp = lp.enterlogindetails(username, password);
+
+		// UserManagement_page
 		Thread.sleep(3000);
-        vp.clickonadmin();
-        vp.adduser();
-        jp=vp.System_user();
-        
-        //jobpage
-        pg= jp.jobtitle();
-		emp=pg.paygradepage();
-		jc=emp.Employement_Statuspage();
-		ws=jc.Job_Categories();
-		
+		vp.clickonadmin();
+		vp.adduser();
+		jp = vp.system_user();
+	}
+
+	@Test
+	public void jobpageflow() throws Exception {
+		// jobpage
+		pg = jp.jobtitle();
+		emp = pg.paygradepage();
+		jc = emp.employement_Statuspage();
+		ws = jc.job_Categories();
+
 		Properties prop = new Properties();// this method is already there in util class
-	    FileInputStream fs =new FileInputStream(System.getProperty("user.dir")+("\\src\\testData\\GlobalInput.properties"));
-	    prop.load(fs);
-	    String starting_time_am_pm = prop.getProperty("starting_time_am_pm");				
-	    String Ending_time_am_pm = prop.getProperty("Ending_time_am_pm");
-	 	    
-	    gp=ws.Work_Shifts(starting_time_am_pm,Ending_time_am_pm);
-	    
-	    //Organization
-	    lo= gp.Organization_generalInfo();
-	    sp=lo.locations();
-	    qf=sp.structurepage();
-	  
-	    //Qualification
-	    ep=qf.addskills();
-	    el=ep.EducationPage();
-		ql=el.licensespage();
-		qm=ql.languagepage();
-		nl=qm.Membershippage();
-		
-		//Nationality
-		cb=nl.nationalities();
-		
-		//corporate-branding
-		ec=cb.CorporateBrandingPage();
-		
-		//configuration
+		FileInputStream fs = new FileInputStream(
+				System.getProperty("user.dir") + ("\\src\\testData\\GlobalInput.properties"));
+		prop.load(fs);
+		String starting_time_am_pm = prop.getProperty("starting_time_am_pm");
+		String Ending_time_am_pm = prop.getProperty("Ending_time_am_pm");
+
+		gp = ws.work_Shifts(starting_time_am_pm, Ending_time_am_pm);
+	}
+
+	@Test(dependsOnMethods = { "adminmenuflow", "jobpageflow" })
+	public void organizationflow() throws Exception {
+		lo = gp.organization_generalInfo();
+		sp = lo.locations();
+		qf = sp.structurepage();
+	}
+
+	@Test(dependsOnMethods = { "adminmenuflow", "jobpageflow", "Organizationflow" })
+	public void qualificationflow() throws Exception {
+		// Qualification
+		ep = qf.addskills();
+		el = ep.educationPage();
+		ql = el.licensespage();
+		qm = ql.languagepage();
+		nl = qm.membershippage();
+	}
+
+	@Test(dependsOnMethods = { "adminmenuflow", "jobpageflow", "Organizationflow", "qualificationflow" })
+	public void nationalityflow() throws Exception {
+		// Nationality
+		cb = nl.nationalities();
+	}
+
+	@Test(dependsOnMethods = { "adminmenuflow", "jobpageflow", "Organizationflow", "qualificationflow",
+			"nationalityflow" })
+	public void corporate_brandingflow() throws Exception {
+		// corporate-branding
+		ec = cb.corporateBrandingPage();
+
+	}
+
+	@Test(dependsOnMethods={"adminmenuflow","jobpageflow","Organizationflow","qualificationflow","nationalityflow","corporate_brandingflow"})
+       public void configurationflow() throws Exception {
 		es=ec.emailconfiguration();
 		lc=es.emailsubscription();
 		lang = lc.localization();
 		mc=lang.language_pakage();
 		rc=mc.module();
-		ldap= rc.OAuthClient_registration();
-		cs=ldap.Config();
-		
+		ldap= rc.outhClient_registration();
+		cs=ldap.config();
+	       
+       	@Test(dependsOnMethods={"adminmenuflow","jobpageflow","Organizationflow","qualificationflow","nationalityflow","corporate_brandingflow","configurationflow"})
+       public void closebrowserflow() throws Exception {
 		//closebrowser
 		cs.closebrowser();
 		
 	}
-		
-		
-	}
+
+}
 
